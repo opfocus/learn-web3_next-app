65c25f18c2e95510cce899d0b822550338298a6b
simple8720 <wangzhsr@gmail.com>
2023-11-07 20:01:00 +0800

    account_view UI

--- app/bridge/account/deposit/page.tsx
+++ app/bridge/account/deposit/page.tsx
@@ -2,9 +2,65 @@
 function DepositPage() {
 
   return (
-    <div>
-      hello
-    </div>
+    <div className="px-9 py-8 w-full border-t border-gray-200">
+      <table className="table-auto  w-full">
+        <thead className=" text-left">
+          <tr>
+            <th className="py-2">
+              TIME
+            </th>
+            <th className="py-2">
+              TYPE
+            </th>
+            <th className="py-2">
+              ANOUNT
+            </th>
+            <th className="py-2">
+              TRANSACTION
+            </th>
+            <th className="py-2">
+              STATUS
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border-t border-gray-200">
+            <td className="py-2">
+              2023年10月17日 GMT+8 23:11
+            </td>
+            <td className="py-2">
+              Deposit
+            </td>
+            <td className="py-2">
+              0.1 ETH
+            </td>
+            <td className="py-2">
+              0xcbec...e262
+            </td>
+            <td className="py-2">
+              Complete
+            </td>
+          </tr>
+          <tr className=" border-t border-gray-200">
+            <td className="py-2">
+              2023年10月17日 GMT+8 23:11
+            </td>
+            <td className="py-2">
+              Deposit
+            </td>
+            <td className="py-2">
+              0.1 ETH
+            </td>
+            <td className="py-2">
+              0xcbec...e262
+            </td>
+            <td className="py-2">
+              Complete
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div >
   )
 }
 
--- app/components/Header/Search/Search.tsx
+++ app/components/Header/Search/Search.tsx
@@ -3,8 +3,8 @@ function Search() {
   return (
     <div className="grow py-2  flex place-content-center ">
       <div className="grow p-2 flex  flex-row gap-2 items-center  px-2.5 rounded-full bg-gray-100 max-w-screen-sm" style={{ height: '100%' }} >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
         </svg>
 
         <input className="grow py-1 bg-inherit focus:outline-none " type="text" />
